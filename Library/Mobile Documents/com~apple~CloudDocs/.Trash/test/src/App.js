7be0436bed17f02f59be983fdfe4084d02a970ac
Donald Ugobude <ugobudedonald@gmail.com>
2021-06-28 14:06:57 +0100

    initial commit

---
+++ Library/Mobile Documents/com~apple~CloudDocs/.Trash/test/src/App.js
@@ -0,0 +1,14 @@
+import './App.scss';
+import Second from './components/a/b/c/Second';
+import First from './components/a+/First';
+
+function App() {
+  return (
+    <div>
+      <Second />
+      <First />
+    </div>
+  );
+}
+
+export default App;
